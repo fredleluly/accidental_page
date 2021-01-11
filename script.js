9efb5c1cfca3e63811af2cbb94fc5fa7a21be0d7
fredleluly <fredleluly@gmail.com>
2021-01-12 05:30:03 +0700

    sudah jadi heheh

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.navbar')    
+    const loss = document.querySelector('.logos')    
+    menuToggle.classList.toggle('show')
+    loss.classList.toggle('logoss')
+}

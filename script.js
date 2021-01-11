8445c72ece064edbb4f13f276ab9aa1fd584eabc
fredleluly <fredleluly@gmail.com>
2021-01-12 05:55:17 +0700

    fix bug

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
     menuToggle.classList.toggle('show')
     loss.classList.toggle('logoss')
 }
+
+const linkcolor = document.querySelectorAll('.nav-link')
+function activelink(){
+    linkcolor.forEach(Link => {Link.classList.remove('active')})
+    // console.log(this.classList.contains('active'))
+    this.classList.add('active')
+}
+linkcolor.forEach(Link => Link.addEventListener('click',activelink))
